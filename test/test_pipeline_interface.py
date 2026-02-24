a834af1799e8936b5aea0e85394acfaaa92cae52
XciciciX <93366526+XciciciX@users.noreply.github.com>
2026-02-23 17:15:33 -0800

    fix test

--- test/test_pipeline_interface.py
+++ test/test_pipeline_interface.py
@@ -189,7 +189,7 @@ class TestPipelineInterface(unittest.TestCase):
     @patch("pipeline_interface.CodeQuality", FakeCodeQuality)
     def test_call_reviewer_increments_review_count_on_error(self):
         class DummyReviewer:
-            def test_code(self, code, tool, package_name, n_features=None):
+            def test_code(self, code, tool, package_name, n_features=None, train_dataset=None):
                 return "error"
 
         state = pipeline_interface.build_state()
